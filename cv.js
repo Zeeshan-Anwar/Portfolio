fefb114bc8f97c43bdc754c4c6e690875fc82edc
Zeeshan-Anwar <69687620+Zeeshan-Anwar@users.noreply.github.com>
2020-11-20 16:53:12 +0500

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,11 @@
+
+	<script>
+	for (var i = 0;i++) {
+		var a = "Software Engineer";
+		consol.log.getElementbyId(change).innerhtml=a;
+		delay: 4 sec;
+		var b = "Web Devolper";
+		consol.log.getElementbyId(change).innerhtml=b;
+
+	}
+	</script>
